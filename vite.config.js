76bbc9c63d711a019183057b1fa1ae021111276b
renato <renatovisouza@gmail.com>
2023-10-19 01:51:17 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return defineConfig({
+        server: {
+            host: '0.0.0.0',
+            hmr: {
+                host: process.env.VITE_APP_HOST,
+                clientPort: 5174,
+                protocol: 'ws',
+            },
+            port: 5174,
+            watch: {
+                usePolling: true,
+            }
+        },
+        plugins: [
+            laravel({
+                input: [
+                    'resources/css/app.css',
+                    'resources/js/app.js',
+                ],
+                refresh: true,
+            }),
+        ],
+    });
+};
